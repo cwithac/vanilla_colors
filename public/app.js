8f343004afd19d213e671463073d274cc0537e4e
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-04 12:27:16 -0500

    Initial color selected verification logic

--- public/app.js
+++ public/app.js
@@ -8,7 +8,19 @@ const colors = [
 ];
 
 const squares = document.querySelectorAll('.square');
+const selectedColor = colors[4];
+const rgbDisplay = document.getElementById('colorDisplay');
+
+rgbDisplay.textContent = selectedColor;
 
 for (let i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
+  squares[i].addEventListener('click', (e) =>{
+    let clickedColor = e.currentTarget.style.backgroundColor;
+    if (clickedColor === selectedColor) {
+      console.log('Correct');
+    } else {
+      console.log('Wrong');
+    }
+  });
 };
