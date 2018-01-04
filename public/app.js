565dd2ddb52a8ee417641383169626d31c3a13f8
Cathleen Wright <cathleenmwright@gmail.com>
2018-01-04 13:27:17 -0500

    Color change to correct color upon win, code commenting

--- public/app.js
+++ public/app.js
@@ -14,15 +14,24 @@ const messageDisplay = document.querySelector('#message');
 
 rgbDisplay.textContent = selectedColor;
 
+//Sets click listeners and conditionals for clicked squares
 for (let i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener('click', (e) =>{
     let clickedColor = e.currentTarget.style.backgroundColor;
     if (clickedColor === selectedColor) {
       messageDisplay.textContent = 'Correct! You Win!';
+      changeColors(clickedColor);
     } else {
       e.currentTarget.style.backgroundColor = '#232323';
       messageDisplay.textContent = 'Incorrect, Try Again!';
     }
   });
+};
+
+//Changes all squares to clicked color
+const changeColors = (color) => {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
 };
